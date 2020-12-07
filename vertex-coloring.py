4defb3109e303bffd64f027d9ac45e8e889a8a64
Jay Van Dam <jvd@macbook-pro-jvd.lan>
2020-12-07 04:05:11 -0500

    found a problem with getting correct coloring to draw, may have fixed it

--- vertex-coloring.py
+++ vertex-coloring.py
@@ -1,18 +1,16 @@
+# Week 14 Assignment
+# NP-Complete: Vertex Coloring
+# Jay Van Dam, Dec 2020
+
 import itertools
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
-def factorial(n): # positive integers only n>=0
-    fact = 1
-    for num in range(2, n + 1):
-        fact *= num
-    return fact
 
 # Python function is_proper(g,c) passes two parameters, a graph in the form of a
-# dictionary (g) and a colo# dictionary (g) and a coloring of this graphs vertices,
-# also in the form of aring of this graphs vertices, also in the form of a
-# dictionary (c). This function transcribes the LETTERS of the given
+# dictionary (g) and a coloring of this graphs vertices, also in the form of a
+# dictionary (c). This function transcribes the NUMBERS/LETTERS of the given
 # graph into their corresponding COLORS/NUMBERS using a nested FOR loop. One
 # for accessing each individual vertex-string (keys) and the adjacent-list of
 # vertices they are paired with (values). An IF statement checks to see if
@@ -20,7 +18,7 @@ def factorial(n): # positive integers only n>=0
 # each list of adjacent edges and looks at the individual vertex strings.
 # Again an IF statement checks to see if the vertex exists in the given
 # coloring. If so, it builds a temporary list of these adjacent vertices as
-# their color/number list newAdj. Once looped through all adjacents, we index
+# their color/number list newAdj. Once looped through all adjacencies, we index
 # this list with the color/number of the vertex they are adjacent for.
 #
 # The last FOR loop simply uses the same logic as above opening up lists etc.
@@ -91,7 +89,7 @@ def greedy(g, o):
                 while loop is True:
                     loop = False
                     for y in g[x]:
-                        if c[y] == c[x]:
+                        if c[x] == c[y]:
                             c[x] = c[y] + 1
                             loop = True
         greedyColoring = {}
@@ -104,45 +102,137 @@ def greedy(g, o):
     else:
         return {}
 
+##############################################
+# Brute-force, greediest graph coloring finder
+##############################################
 
-# Brute-force greediest coloring finder
 
-NODES = 9 # number of nodes
-P = 0.5 # probability for edge creation
 
-gen_nodes = [n for n in range(NODES)]
-G = nx.erdos_renyi_graph(NODES,P) # random graph generator
+NODES = 6 # choose number of nodes
+P = 0.5 # choose probability for edge creation
+
+# Random graph generator provided by Networkx
+# https://networkx.org
+G = nx.erdos_renyi_graph(NODES,P)
 gen_graph = nx.to_dict_of_lists(G)
 
+print(gen_graph)
+
+# list of all nodes for ordering
+gen_nodes = [n for n in range(NODES)]
 permutations = list(itertools.permutations(gen_nodes))
 
 coloring = []
 chromatic_num = []
 order = []
 greediest = []
+color_map = []
 
+# Find greedy colorings for all orderings
 for p in permutations:
-    coloring.append(greedy(gen_graph,p))
-    chromatic_num.append(max(coloring[-1].values())) # find "largest" color with ordering, -1 = last element
-    order.append(p)
+    c = greedy(gen_graph, p)
+    if c: # if dictionary isn't empty
+        coloring.append(c)
+        chromatic_num.append(max(coloring[-1].values())) # find "largest" color with ordering, -1 = last element
+        order.append(p)
+       # print(chromatic_num[-1], order[-1], coloring[-1])
 
-greediest.append(min(chromatic_num))
-if chromatic_num.count(greediest[-1]) > 1:
-    for i in [i for i, j in enumerate(chromatic_num) if j == min(chromatic_num)]:
-        greediest.append(i)
+print(min(chromatic_num))
+print([i for i, j in enumerate(chromatic_num) if j == min(chromatic_num)])
 
-pos = nx.random_layout(G) #organizes graph better
-color_map = [coloring[0].get(node, 0.25) for node in G.nodes()]
+# if there is more than one coloring with smallest chromatic number, append the rest
+for x in [i for i, j in enumerate(chromatic_num) if j == min(chromatic_num)]:
+    greediest.append(x)
+
+pos = nx.random_layout(G) # organizes graph better
+# color_map = [greediest[0].get(node, 0.1) for node in G.nodes()] # distributes different colors over gradient
+
+for node in G:
+    print(coloring[greediest[0]].get(node))
+    if (coloring[greediest[0]].get(node) == 1):
+        color_map.append('red')
+    elif (coloring[greediest[0]].get(node) == 1):
+        color_map.append('green')
+    elif (coloring[greediest[0]].get(node) == 2):
+        color_map.append('blue')
+    elif (coloring[greediest[0]].get(node) == 3):
+        color_map.append('yellow')
+    elif (coloring[greediest[0]].get(node) == 4):
+        color_map.append('orange')
+    elif (coloring[greediest[0]].get(node) == 5):
+        color_map.append('pink')
+    elif (coloring[greediest[0]].get(node) == 6):
+        color_map.append('grey')
+    else:
+        color_map.append('black')
+
+
+# draw "greediest" colored graph
 nx.draw(G, cmap=plt.get_cmap('hsv'), node_color=color_map, with_labels=True, font_color='white')
+print(gen_graph)
 
 # adds padding to prevent node clipping
 ax = plt.gca()
 ax.margins(0.20)
 
-plt.savefig("optimal_vertex_coloring-4.png") # save as png
-plt.show()
+plt.savefig("optimal_vertex_coloring-8.png") # save as png
+plt.show() # display graph
 
-for g in greediest:
-    print(chromatic_num[g], order[g], coloring[g])
+# print parameters for drawn graph
 
 print(chromatic_num[0], order[0], coloring[0])
+
+# gen_graph = {"A":["M","B","E","C"],
+#             "B":["M","A","E","D"],
+#             "C":["A","E","N","F"],
+#             "D":["B","G"],
+#             "E":["C","A","B","G","H","N"],
+#             "F":["C","H","O","I"],
+#             "G":["E","D","P","J"],
+#             "H":["O","F","E","J","L","K"],
+#             "I":["F","K"],
+#             "J":["L","H","G","P"],
+#             "K":["I","H","L","Q"],
+#             "L":["Q","K","H","J"],
+#             "M":["A","B"],
+#             "N":["C","E"],
+#             "O":["F","H"],
+#             "P":["G","J"],
+#             "Q":["K","L"]}
+#
+# gen_nodes = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+#               "N","O","P","Q"]
+
+
+#            gen_graph = { "A":["M","B","E","C","Z","S"],
+#            "B":["M","A","E","D"],
+#            "C":["A","E","N","F"],
+#            "D":["B","G"],
+#            "E":["C","A","B","G","H","N","R"],
+#            "F":["C","H","O","I"],
+#            "G":["E","D","P","J"],
+#            "H":["O","F","E","J","L","K"],
+#            "I":["F","K"],
+#            "J":["L","H","G","P"],
+#            "K":["I","H","L","Q"],
+#            "L":["Q","K","H","J"],
+#            "M":["A","B"],
+#            "N":["C","E"],
+#            "O":["F","H"],
+#            "P":["G","J"],
+#            "Q":["K","L"],
+#            "R":["E"],
+#            "S":["A","T"],
+#            "T":["S","U"],
+#            "U":["T","V"],
+#            "V":["U","W"],
+#            "W":["V","X"],
+#            "X":["W","Y"],
+#            "Y":["X","Z"],
+#            "Z":["A","Y"]}
+
+#gen_nodes = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+#              "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+# gen_graph = {"A": ["B", "E"], "B": ["A", "C", "E"], "C": ["B", "D", "E"], "D": ["C"], "E" : ["A", "B", "C"]}
+# gen_nodes = ["A","B","C","D","E"]
